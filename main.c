51b6aec5f73c83309b4f951f4d0acdfb8a4584f8
Zatomate <duthoit.thomas9@gmail.com>
2025-06-29 12:01:30 +0200

    begin refactor
    refactoring the entire project to be built around the uSD card for games execution

--- main.c
+++ main.c
@@ -1,19 +1,9 @@
-#include "os.h"
+#include "init_hardware.h"
 
 int main() {
     stdio_init_all();
     
-    os_init();
-
-    sleep_ms(1000);  // 1000 ms d'attente pour voir le boot screen
-
-    os_main_menu();
-
-    sleep_ms(1000);
-    
-    // os_run_game(GAMES.games[0]);
-
-    // g_fill_rect(0, 0, SCREEN_WIDTH-1, SCREEN_HEIGHT-1, CYAN);
+    init_hardware();
 
     while (1) {
         tight_loop_contents();
